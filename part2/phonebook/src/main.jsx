306693e7548ad7e9ee8bbf5061b8b389c98d67c6
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-04-05 09:32:33 +0600

    Exercises 3.1 - 3.12 completed

--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import axios from 'axios'
 
-axios.get('http://localhost:3001/persons').then(response => {
+axios.get('http://localhost:3001/api/persons').then(response => {
     const phonebook = response.data;
     ReactDOM.createRoot(document.getElementById('root')).render(<App phonebook={phonebook} />)
 })
