5ade4b1e03929099f8b4616e7fae4a0aefa317e1
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-04-07 17:35:06 +0600

    tests for bloglist has added

---
+++ part4/blogList-server/test/list.test.js
@@ -0,0 +1,180 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+const blogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }  
+]
+
+describe('total likes', () => { 
+  test('of empty list is zero', () => { 
+    const blogs = []
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one block equals the likes of that', () => { 
+    const blogs = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+      }
+    ]
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 7)
+  })
+  test('of a bigger list calculated right', () => { 
+      const result = listHelper.totalLikes(blogs)
+      assert.strictEqual(result, 36)
+   })
+})
+
+describe("Most Liked Blog", () => {
+  test("when list has only one blog, return that blog back", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+      }
+    ]
+    const result = listHelper.favoriteBlog(blogs);
+	  assert.deepStrictEqual(result, {
+		title: "Go To Statement Considered Harmful",
+		author: "Edsger W. Dijkstra",
+		likes: 5
+	  })
+	})
+
+	test("when list has many blogs, return the most liked blog", () => {
+	  const result = listHelper.favoriteBlog(blogs);
+	  assert.deepStrictEqual(result, {
+	  	title: "Canonical string reduction",
+	  	author: "Edsger W. Dijkstra",
+	  	likes: 12,
+	  })
+	})
+
+	test("when list has no blogs, nothing should be returned", () => {
+	  const result = listHelper.favoriteBlog([]);
+	  assert.deepStrictEqual(result, {})
+	});
+});
+
+describe("Author who has the largest amount of blogs", () => {
+  test("of empty list is {}", () => {
+	const resultEmptyBlogList = listHelper.mostBlogs([]);
+    assert.deepStrictEqual(resultEmptyBlogList, {})
+  });
+
+  test("when list has only one blog equals to the author of that blog", () => {
+  	const blog = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+      }
+    ];
+  	const mostBlogs = listHelper.mostBlogs(blog);
+    assert.deepStrictEqual(mostBlogs, {
+        author: "Michael Chan",
+        blogs: 1
+    })
+  });
+
+  test("of a bigger list is calculated right", () => {
+  	const mostBlogs = listHelper.mostBlogs(blogs);
+  	assert.deepStrictEqual(mostBlogs, {
+  	  author: "Robert C. Martin",
+  	  blogs: 3,
+  	});
+  });
+});
+
+describe("Author whose blog posts have the largest amount of likes", () => {
+  test("of empty list is {}", () => {
+  	const resultEmptyBlogList = listHelper.mostLikes([]);
+  	assert.deepStrictEqual(resultEmptyBlogList, {});
+  });
+
+  test("when list has only one blog equals to the author of that blog", () => {
+  	const blog = [  {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+      }];
+  	const mostBlogs = listHelper.mostLikes(blog);
+  	assert.deepStrictEqual(mostBlogs, {
+  	  author: mostBlogs.author,
+  	  likes: 7,
+  	});
+  });
+  
+  test("of a bigger list is calculated right", () => {
+	const mostBlogs = listHelper.mostLikes(blogs);
+	assert.deepStrictEqual(mostBlogs, {
+	  author: "Edsger W. Dijkstra",
+	  likes: 17,
+	});
+	});
+});
