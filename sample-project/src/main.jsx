e8db363b8bb58d87baf4758a2febe564343554ec
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-04-08 05:56:09 +0600

    Sample project bugs fixed in part 3

--- sample-project/src/main.jsx
+++ sample-project/src/main.jsx
@@ -1,9 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import axios from 'axios'
+import App from './App'
+import './index.css'
 
-axios
-    .get('/api/notes').then(response => {
-        const notes = response.data
-        ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
-})
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
