85b0211605c212f52c8f893e60cbd4d800383569
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-04-15 23:33:56 +0600

    Phonebook Application stage 2 completed

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const personObject = {
+      name: newName,
+      id: String(persons.length + 1)
+    }
+
+    const allNames = persons.map(person => person.name);
+
+    if (allNames.includes(newName)) {
+      window.alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+
+      {persons.map(person => (
+        <p key={person.id}>{person.name}</p>
+      ))}
+
+    </div>
+  )
+}
+
+export default App
