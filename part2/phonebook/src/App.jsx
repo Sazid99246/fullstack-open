064c93167098cec722d2ef0021f6e59218a4e9b0
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-04-16 08:09:54 +0600

    moved components folders in src folder, and added json-server to the notes app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import Filter from '../components/Filter'
-import PersonForm from '../components/PersonForm'
-import Persons from '../components/Persons'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
