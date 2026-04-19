d2401b84524007cc97e24228c69e29ecd272d7eb
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-04-19 17:02:23 +0600

    backend full functionality added for the apps phonebook and notes

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -3,28 +3,26 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose
-  .connect(url)
-  .then((result) => {
+mongoose.connect(url, { family: 4 })
+  .then(result => {
     console.log('connected to MongoDB')
   })
-  .catch((error) => {
+  .catch(error => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchem = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-noteSchema.set('toJSON', {
+personSchem.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  },
+  }
 })
 
-module.exports = mongoose.model('Note', noteSchema)
+
+module.exports = mongoose.model('Person', personSchem)
