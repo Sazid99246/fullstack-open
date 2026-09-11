8c064f4fed82b8828417fe02aa884d28632dd655
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-09-11 17:22:48 +0600

    Notes server implemented

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react"
 import Note from "./components/Note"
+import { useEffect } from "react";
+import axios from "axios";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -21,10 +35,6 @@ const App = (props) => {
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
   }
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
 
   return (
     <div>
