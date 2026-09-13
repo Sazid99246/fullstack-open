85366f3504bf13f839bff34456278e14cbccc392
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-09-13 17:32:25 +0600

    Phonebook full stack app completed with validation

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -118,6 +118,12 @@ const errorHandler = (error, request, response, next) => {
     })
   }
 
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message
+    })
+  }
+
   next(error)
 }
 
--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -14,8 +14,22 @@ mongoose.connect(url, { family: 4 })
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: value => {
+        return /^\d{2,3}-\d+$/.test(value)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
