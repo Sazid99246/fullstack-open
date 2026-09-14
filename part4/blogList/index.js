d9b1e7c401895a75abc135c181f601e1fb59d2dc
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-09-14 18:49:47 +0600

    Blog model refactored

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,26 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const logger = require('./utils/logger')
-const config = require('./utils/config')
-
 const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const Blog = require('./models/blog');
 
 const mongoUrl = config.MONGO_URI
 mongoose.connect(mongoUrl, { family: 4 })
