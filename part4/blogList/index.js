4c42375c7f215a8a2be9fed715cb59438d1720ae
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-09-14 19:08:15 +0600

    Blog list app fully refactored

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,35 +1,6 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const Blog = require('./models/blog');
-
-const mongoUrl = config.MONGO_URI
-mongoose.connect(mongoUrl, { family: 4 })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+const app = require('./app')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
