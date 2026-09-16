e685685f514941c3b29c8f8f31118d3f285d10ed
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-09-16 10:23:51 +0600

    added user route in phonebook

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -4,7 +4,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
-
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -18,8 +18,7 @@ mongoose.connect(config.MONGO_URI, { family: 4 })
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.requestLogger)
-
-
 
 module.exports = app
